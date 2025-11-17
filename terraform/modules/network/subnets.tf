fc532c7d133e142fec214f5530ad15c427390008
Rayan-Wong <work.rayanwong@gmail.com>
2025-11-17 15:16:38 +0800

    Defined AZs for subnets

--- terraform/modules/network/subnets.tf
+++ terraform/modules/network/subnets.tf
@@ -1,7 +1,9 @@
 resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.main.id
-  for_each   = var.public_subnets
-  cidr_block = each.value
+  for_each = var.public_subnets
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = each.value.cidr
+  availability_zone = "${var.aws_region}${each.value.az}"
   tags = merge(
     var.tags,
     {
