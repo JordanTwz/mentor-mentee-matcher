f055f8e61544e57e37c644c664a169fe58d6079f
Rayan-Wong <134691628+Rayan-Wong@users.noreply.github.com>
2025-11-19 14:28:32 +0000

    Updated TG to use name prefix

--- terraform/modules/alb/target_group.tf
+++ terraform/modules/alb/target_group.tf
@@ -1,5 +1,5 @@
 resource "aws_lb_target_group" "default" {
-  name        = "public-tg"
+  name_prefix = "public-tg-"
   port        = var.app_port
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
